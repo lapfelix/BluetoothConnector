63d3b3e767f231a6a4cbb19f00cce0d7d2506d22
lapfelix <felix@lapal.me>
2017-02-14 00:08:22 -0500

    List paired devices after printing help

--- BluetoothConnector.swift
+++ BluetoothConnector.swift
@@ -3,7 +3,14 @@ import IOBluetooth
 let hardcodedAddress : String? = nil//"12-34-56-78-90-ab"
 
 func printHelp() {
-    print("Usage:\nBluetoothConnector 00-00-00-00-00-00");
+    print("Usage:\nBluetoothConnector 00-00-00-00-00-00\n\nGet the MAC address from the list below (if your device is missing, pair it with your computer first):");
+    IOBluetoothDevice.pairedDevices().forEach({(device) in
+        guard let device = device as? IOBluetoothDevice,
+        let addressString = device.addressString,
+        let deviceName = device.name
+        else { return }
+        print("\(addressString) - \(deviceName)")
+    })
 }
 
 if (CommandLine.arguments.count == 2 || hardcodedAddress != nil) {

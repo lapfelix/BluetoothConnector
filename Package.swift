ebcd34e959c11f03545c5d511f730c66a431b75e
Felix Lapalme <felix@lapal.me>
2018-11-27 23:22:29 -0500

    Update SimpleCLI dependency URL to https

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "BluetoothConnector",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:lapfelix/SimpleCLI.git", from: "0.1.0"),
+        .package(url: "https://github.com/lapfelix/SimpleCLI.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

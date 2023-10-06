e2060d5c5a8a9b0427b9dcd0737d9d053ca4fc67
lapfelix <felix@lapal.me>
2023-10-06 11:59:27 -0400

    Update swift-tools-version & swift-argument-parser to latest versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("1.1.2"))
+        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.2.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
